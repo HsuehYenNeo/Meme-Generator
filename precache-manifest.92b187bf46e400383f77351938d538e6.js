657f9f48f38697905f5d842b7d8998a10359e71a
Hangrykat <Hangrykat@users.noreply.github.com>
2020-08-14 10:27:48 +0200

    Added build for static hosting

---
+++ precache-manifest.92b187bf46e400383f77351938d538e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8da4d1cdf8a810a93dc31dcea06d55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24866cd9a17030a2254b",
+    "url": "/static/css/main.8c8f12dc.chunk.css"
+  },
+  {
+    "revision": "fd95995ae5ad7e016fe9",
+    "url": "/static/js/2.00b5af86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00b5af86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24866cd9a17030a2254b",
+    "url": "/static/js/main.50330890.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  }
+]);
